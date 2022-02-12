f81e091a1768cc9923bd8b488e8765a5f61baa2f
Gonzalodr2003 <98487271+Gonzalodr2003@users.noreply.github.com>
2022-02-12 16:53:52 +0100

    documentada clase Persona

--- doc/type-search-index.js
+++ doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.iescomercio.ed.bloque2.repaso.modelo","l":"Curso"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.iescomercio.ed.bloque2.repaso.modelo","l":"Curso"},{"p":"com.iescomercio.ed.bloque2.repaso.modelo","l":"Persona"}]
--- src/com/iescomercio/ed/bloque2/repaso/modelo/Persona.java
+++ src/com/iescomercio/ed/bloque2/repaso/modelo/Persona.java
@@ -1,16 +1,38 @@
 package com.iescomercio.ed.bloque2.repaso.modelo;
 
+/**
+ * Clase que representa una persona en la que se puede meter a una persona 
+ * introduciendo x parámetros, sacar e introducir su DNI, sacar e introducir su nombre, 
+ * sacar e introducir su apellido, comparar y pararlo todo a String.
+ * 
+ * @author Carlos Sáenz Adán
+ * @author Gonzalo Díaz Romeo
+ * @version 1.0
+ * @since 1.0
+ *
+ */
 public class Persona{
 	
 	private String dni;
 	private String nombre;
 	private String apellido1;
 	
+	/**
+	 * Constructor de la clase Persona sin parámetros (Esta es la default)
+	 */
 	public Persona() {}
 	
 	/*
 	 * Documentar y crear test unitario
 	 * */
+	/**
+	 * Constructor de la clase persona, en la que se introducen parámetros para darle forma a la clase 
+	 * en ese momento.
+	 * 
+	 * @param dni DNI de la persona que queremos introducir
+	 * @param nombre Nombre de la persona que queremos introducir
+	 * @param apellido1 Primer apellido de la persona que queremos introducir
+	 */
 	public Persona(String dni, String nombre, String apellido1) {
 		this.dni = dni;
 		this.nombre = nombre;
@@ -20,6 +42,10 @@ public class Persona{
 	/*
 	 * Documentar y crear test unitario
 	 * */
+	/**
+	 * Método que devuelve el DNI de la persona
+	 * @return El DNI de la persona
+	 */
 	public String getDni() {
 		return dni;
 	}
@@ -28,6 +54,11 @@ public class Persona{
 	/*
 	 * Documentar y crear test unitario
 	 * */
+	/**
+	 * Método que te permite introducir el DNI de la persona.
+	 * @param dni DNI de la persona que queremos introducir.
+	 * @throws Exception Exception Si el último dígito del DNI no es una letra.
+	 */
 	public void setDni(String dni) throws Exception {
 		//comprobacion de si el ultimo caracter es una letra
 		if(Character.isLetter(dni.charAt(dni.length()-1))) {
@@ -40,6 +71,10 @@ public class Persona{
 	/*
 	 * Documentar y crear test unitario
 	 * */
+	/**
+	 * Método que devuelve el nombre de la persona.
+	 * @return El nombre de la persona
+	 */
 	public String getNombre() {
 		return nombre;
 	}
@@ -47,6 +82,10 @@ public class Persona{
 	/*
 	 * Documentar y crear test unitario
 	 * */
+	/**
+	 * Método que te permite introducir el nombre de la persona.
+	 * @param nombre Nombre de la persona.
+	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
@@ -54,6 +93,10 @@ public class Persona{
 	/*
 	 * Documentar y crear test unitario
 	 * */
+	/**
+	 * Método que devuelve el apellido de la persona.
+	 * @return El apellido de la persona.
+	 */
 	public String getApellido1() {
 		return apellido1;
 	}
@@ -61,6 +104,10 @@ public class Persona{
 	/*
 	 * Documentar y crear test unitario
 	 * */
+	/**
+	 * Método que te permite introducir el apellido de la persona.
+	 * @param apellido1 Primer apellido de la persona.
+	 */
 	public void setApellido1(String apellido1) {
 		this.apellido1 = apellido1;
 	}

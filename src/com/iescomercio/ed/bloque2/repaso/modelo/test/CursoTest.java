3db5b113063e609246cccf8a3a8699657b1085f7
Gonzalodr2003 <98487271+Gonzalodr2003@users.noreply.github.com>
2022-02-12 14:56:10 +0100

    implementacion test

--- src/com/iescomercio/ed/bloque2/repaso/modelo/test/CursoTest.java
+++ src/com/iescomercio/ed/bloque2/repaso/modelo/test/CursoTest.java
@@ -4,11 +4,31 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import com.iescomercio.ed.bloque2.repaso.modelo.Curso;
+import com.iescomercio.ed.bloque2.repaso.modelo.Persona;
+
 class CursoTest {
 
 	@Test
 	void testEliminarAlumno() {
-		fail("Not yet implemented");
+		Integer cantSinEliminar;
+		Integer cantEliminando;
+		Persona persona1 = new Persona ("15784512F", "Gonzalo", "Diaz");
+		Persona persona2 = new Persona ("15785512F", "Raul", "Romeo");
+		Persona persona3 = new Persona ("18785512F", "Pepe", "Rodriguez");
+		Curso curso1 = new Curso();
+		curso1.aniadirAlumno(persona1);
+		curso1.aniadirAlumno(persona2);
+		curso1.aniadirAlumno(persona3);
+		cantSinEliminar=curso1.numeroAlumnos();
+		try {
+			curso1.eliminarAlumno(persona1.getDni());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		cantEliminando=curso1.numeroAlumnos();
+		assertEquals(cantSinEliminar-1, cantEliminando);
 	}
 
 	@Test
